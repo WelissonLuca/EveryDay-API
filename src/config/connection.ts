bcf0c209fa83b288c6b5405ece32e04ee4d40107
Welisson luca <welissonluca17@gmail.com>
2021-05-09 02:41:11 -0300

    feat: connection mysql established

---
+++ src/config/connection.ts
@@ -0,0 +1,10 @@
+import { createConnection, Connection } from 'typeorm';
+
+const connection = await createConnection({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+});
